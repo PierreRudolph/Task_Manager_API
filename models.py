094d800dd7241c3a9de8a3c74c621b46f2e2b430
PierreRudoph <plettner@hotmail.de>
2026-04-28 12:38:28 +0200

    regiterd first User, added first Task

--- models.py
+++ models.py
@@ -2,5 +2,16 @@ from app import db
 
 
 class User(db.Model):
-        id = db.Column(db.Integer, primary_key=True)  # 👈 DAS ist Pflicht
-        username = db.Column(db.String(80), unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    tasks = db.relationship("Task", backref="user")
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(200))
+    category = db.Column(db.String(50))
+    completed = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
